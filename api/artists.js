bb106b1caa30fece93a411f9a14b46af703f5ed2
mr-t-Durden <67805487+mr-t-Durden@users.noreply.github.com>
2022-01-25 13:55:31 +0100

    Implement route: put /api/artists/:artistId

--- api/artists.js
+++ api/artists.js
@@ -7,6 +7,7 @@ module.exports = artistsRouter;
 
 const db = new sqlite3.Database(process.env.TEST_DATABASE || '../database.sqlite');
 
+// Read all artists
 artistsRouter.get('/', (req, res, next) => {
     db.all(`SELECT * FROM Artist WHERE is_currently_employed = 1`, (err, rows) => {
         if(err) {
@@ -17,6 +18,7 @@ artistsRouter.get('/', (req, res, next) => {
     });
 });
 
+// Handle :artistId -parameter
 artistsRouter.param('artistId', (req, res, next, id) => {
     db.get(`SELECT * FROM Artist WHERE id=$id`, { $id: id }, (err, artist) => {
         if(err) {
@@ -30,10 +32,12 @@ artistsRouter.param('artistId', (req, res, next, id) => {
     }) 
 });
 
+// Read specific artist via Id
 artistsRouter.get('/:artistId', (req, res, next) => {
    res.status(200).json({artist: req.artist}); 
 });
 
+// Create a new artist
 artistsRouter.post('/', (req, res, next) => {
     if( req.body.artist.name && req.body.artist.dateOfBirth && req.body.artist.biography ) {
         if( !req.body.artist.isCurrentlyEmployed ) {
@@ -57,3 +61,30 @@ artistsRouter.post('/', (req, res, next) => {
         res.status(400).send('Missing data (name, date_of_birth or biography)!');
     }
 });
+
+// Update existing artist via id
+artistsRouter.put('/:artistId', (req, res, next) => {
+    if( req.body.artist.name && req.body.artist.dateOfBirth && req.body.artist.biography ) {
+        if( !req.body.artist.isCurrentlyEmployed ) {
+            req.body.artist.isCurrentlyEmployed = 1;
+        }
+        db.run(`UPDATE Artist SET name = $name, date_of_birth = $dateOfBirth, biography = $biography, is_currently_employed = $isCurrentlyEmployed WHERE id = $id`, {
+            $id: req.params.artistId,
+            $name: req.body.artist.name,
+            $dateOfBirth: req.body.artist.dateOfBirth,
+            $biography: req.body.artist.biography,  
+            $isCurrentlyEmployed: req.body.artist.isCurrentlyEmployed
+        }, function(err) {
+            if(err) {
+                next(err);
+            } else {
+                db.get(`SELECT * FROM Artist WHERE id = $id`, { $id: req.params.artistId }, (err, updatedArtist) => {
+                    res.status(200).json({artist: updatedArtist});
+                });
+            }
+        });
+    } else {
+        res.status(400).send('Missing data (name, date_of_birth or biography)!');
+    }
+});
+
