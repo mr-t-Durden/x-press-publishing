6470de3d0c5764c42b6be79c190bdfdb8b1a53d2
mr-t-Durden <67805487+mr-t-Durden@users.noreply.github.com>
2022-01-26 15:05:34 +0100

    Implement route: post /api/series/:seriesId/issues and .issueId parameter handle

--- api/issues.js
+++ api/issues.js
@@ -7,12 +7,59 @@ const issuesRouter = express.Router({mergeParams: true});
 
 module.exports = issuesRouter;
 
+// Read all issues from Issue Table for related series
 issuesRouter.get('/', (req, res, next) => {
     db.all(`SELECT * FROM Issue WHERE series_id = $seriesId`, { $seriesId: req.params.seriesId },(err, rows) => {
         if(err) {
             next(err);
         } else {
             res.status(200).json({issues: rows});
+        }
+    });
+});
+
+// Create new issue for related series
+issuesRouter.post('/', (req, res ,next) => {
+    if ( req.body.issue.name && req.body.issue.issueNumber && req.body.issue.publicationDate && req.body.issue.artistId ) { 
+        // Check for valid artistIdissue.
+        db.get(`SELECT * FROM Artist WHERE id = $artistId`, { $artistId: req.body.issue.artistId }, (err, artist) => {
+            if(!artist) {
+                res.status(400).send('Invalid artist-ID!');
+            } 
+        });
+
+        db.run(`INSERT INTO Issue (name, issue_number, publication_date, artist_id, series_id) VALUES ($name, $issueNumber, $publicationDate, $artistId, $seriesId)`, {
+            $name: req.body.issue.name,
+            $issueNumber: req.body.issue.issueNumber,
+            $publicationDate: req.body.issue.publicationDate,
+            $artistId: req.body.issue.artistId,
+            $seriesId: req.params.seriesId
+        }, function(err) {
+            if(err) {
+                next(err);
+            } else {
+                db.get(`SELECT * FROM Issue WHERE id = $id`, { $id: this.lastID }, (err, newIssue) => {
+                    res.status(201).json({issue: newIssue});
+                });
+            }
+        });
+    } else {
+        res.status(400).send('Missing data (name, issueNumber, publicationDate, artistId)!');
+    }
+});
+
+// Handle issueId parameter
+issuesRouter.param('issueId', (req, res, next, id) => {
+    db.get(`SELECT * FROM Issue WHERE id = $issueId`, { $issueId: id }, (err, issue) => {
+        if(err) {
+            next(err);
+        } else {
+            if(issue) {
+                req.issue = issue;
+                next(); 
+            } else {
+                res.status(404).send('Invalid issueId!');
+            }
         }
     });
 });
