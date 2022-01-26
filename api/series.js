2da3e5f4d5db67c4b7aac77cc8d0665f7741f3d1
mr-t-Durden <67805487+mr-t-Durden@users.noreply.github.com>
2022-01-26 11:09:48 +0100

    Implement seriesId parameter handling

--- api/series.js
+++ api/series.js
@@ -18,4 +18,18 @@ seriesRouter.get('/', (req, res, next) => {
     });
 });
 
-
+// Handle parameter :seriesId
+seriesRouter.param('sereisId', (req, res, next, id) => {
+    db.get(`SELECT * FROM Series WHERE id = $id`, { $id: id }, (err, series) => {
+        if(err) {
+            next(err);
+        } else {
+            if( !series ) {
+                res.status(404).send('Invalid Series-ID!');
+            } else {
+                req.series = series;
+                next();
+            }
+        }
+    });
+});
